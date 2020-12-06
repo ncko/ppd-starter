570be0f01a47a73680b9389eebf42432ac82e0e2
Nick Olsen <nolsen@idiq.com>
2020-12-06 14:57:38 -0800

    Update

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { render, fireEvent } from '../testUtils'
+import { render } from '../testUtils'
 import Dashboard from '../../pages/index'
 
 describe('Home page', () => {
   it('clicking button triggers alert', () => {
-    const { getByText } = render(<Dashboard />, {})
+    render(<Dashboard />)
   })
 })
